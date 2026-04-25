6ff0c9c81043b5282b8f36eeb254df9782378b27
kirito <boadatv1@gmail.com>
2026-04-25 11:01:12 +0300

    explore titanic dataset: head, info, describe, age histogram

--- teach pd.py
+++ teach pd.py
@@ -1,1 +1,10 @@
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("titanic.csv")
+
+df['Age'].hist(bins=20)
+plt.title('Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.show()
