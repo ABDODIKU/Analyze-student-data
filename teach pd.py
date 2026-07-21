36352d48a1bb488ca9ce9f019d93857327009dd3
kirito <boadatv1@gmail.com>
2026-07-21 10:09:21 +0300

    Adding dataset I selected

--- teach pd.py
+++ teach pd.py
@@ -1,25 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df= pd.read_csv("titanic_clean.csv")
 
-def age_group(age):
-    if age < 18:
-        return 'child'
-    elif age < 60:
-        return 'adult'
-    else:
-        return 'elderly'
-
-df['AgeGroup'] = df['Age'].apply(age_group)
-print(df['AgeGroup'].value_counts())
-
-print(df.groupby(['AgeGroup','Sex'])['Survived'].mean())
-df.to_csv("titanic_clean.csv", index=False)
-
-
-# df['Age'].hist(bins=20)
-# plt.title('Age Distribution')
-# plt.xlabel('Age')
-# plt.ylabel('Count')
-# plt.show()
+sns.scatterplot(data=df, x='Age', y='Fare', hue='Survived')
+plt.title('Age vs Fare')
+plt.show()
